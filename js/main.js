d502b3b6cb09f9e27963b6fb936150c0bed43798
BailoDev <bailobah137@gmail.com>
2024-02-02 13:37:48 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// ===============================================Scroll Section Active============================================
+function scrollActive(){
+    const scrollY = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 10,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+// ========================xx==================Scroll Section Active=========================xx====================
+
+
+
+// ======================================Change Background Header================================================== 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+// =======================xx=============Change Background Header============================xx=====================
+
+
+
+// ================================================Owl Carousel=====================================================
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            loop:true
+        },
+        600:{
+            items:2,
+            nav:true,
+            loop:true
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+})
+// ====================xx=====================Owl Carousel========================xx================================
+
+
+
+// ==================================================Home Section===================================================
+const btns = document.querySelectorAll(".nav-btn");
+
+let sliderNav = function(manual){
+    btns[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+// ==========================xx======================Home Section=========================xx========================
+
+
