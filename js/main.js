ad08c75c099a7158c4e6d655dd3621d87546ee96
BailoDev <bailobah137@gmail.com>
2024-02-04 17:54:52 +0000

    make sections responsive

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ window.addEventListener('scroll', scrollHeader)
 // ================================================Owl Carousel=====================================================
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:30,
+    margin:15,
     responsiveClass:true,
     responsive:{
         0:{
